190486edecaeb511b7123a627f61c4de2238961b
Marat Yuldashev <maratyv@gmail.com>
2013-10-04 16:00:59 +0400

    Use SortedSet properties to effectively find elemnts instead of traversing

--- src/ru/maratyv/indexer/index/HashMapIndex.java
+++ src/ru/maratyv/indexer/index/HashMapIndex.java
@@ -20,7 +20,7 @@ public class HashMapIndex implements Index {
     @Override
     public synchronized void add(Token token) {
         if (storage.containsKey(token.term)) {
-            Collection<Posting> postings = storage.get(token.term);
+            SortedSet<Posting> postings = storage.get(token.term);
             if (toAddPosting(postings,token.docID)) {
                 postings.add(new Posting(token.docID));
             }
@@ -31,12 +31,14 @@ public class HashMapIndex implements Index {
         }
     }
 
-    private boolean toAddPosting(Collection<Posting> postings, String docID) {
-        for (Posting posting:postings) {
-            if (posting.docID.equals(docID)) {
-                posting.incrementFrequency();
-                return false;
-            }
+    private boolean toAddPosting(SortedSet<Posting> postings, String docID) {
+        SortedSet<Posting> postingsTail = postings.tailSet(new Posting(docID));
+        if (postingsTail == null || postingsTail.isEmpty()) {
+            return true;
+        }
+        if (postingsTail.first().docID.equals(docID)) {
+            postingsTail.first().incrementFrequency();
+            return false;
         }
         return true;
     }
