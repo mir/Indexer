3545101ca08dc88cdb2685f46ade7963bea5d241
Marat Yuldashev <maratyv@gmail.com>
2015-01-23 14:02:08 +0300

    Hotfix

--- src/ru/maratyv/indexer/test/IndexerTest.java
+++ src/ru/maratyv/indexer/test/IndexerTest.java
@@ -36,6 +36,7 @@ public class IndexerTest {
     public void findLorem() throws IOException {
         // number of files containing lorem
         //Antoher test
+        //testtest
         int actual = indexer.find("lorem").size();
         int expected = countFilesWithWords("lorem","lorem");
         assertEquals(expected,actual);
